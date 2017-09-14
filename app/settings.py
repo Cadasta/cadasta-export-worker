67fff59d31b18aeb5ce5324d11dd845c5af93ea1
Anthony Lukach <anthonylukach@gmail.com>
2017-09-13 23:49:08 -0600

    Allow zipstream url to be set via env variable

--- app/settings.py
+++ app/settings.py
@@ -4,4 +4,4 @@ QUEUE = 'export'
 BASE_URL = os.environ.get("BASE_URL", "http://localhost:8000")
 S3_BUCKET = os.environ["S3_BUCKET"]
 
-ZIPSTREAM_URL = "http://localhost:4040"
+ZIPSTREAM_URL = os.environ.get("ZIPSTREAM_URL", "http://localhost:4040")
