8a1aa0ce10a32708329ef9a23cf1b50d4030636f
Anthony Lukach <anthonylukach@gmail.com>
2018-01-24 08:28:16 -0700

    Change header to 'location_type'

--- app/subtasks/xls.py
+++ app/subtasks/xls.py
@@ -40,7 +40,7 @@ def write_locations_sheet(wb, base_url, api_key):
     headers = [
         ('id', 'properties.id'),
         ('geometry.ewkt', 'geometry'),
-        ('tenure_type', 'properties.type'),
+        ('location_type', 'properties.type'),
         ('area', 'properties.area'),
     ]
     header_len = len(headers)
